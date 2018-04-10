1175d2a8de610f1a402e8aecf917d594d5e47165
pavi <MSI-PC@MSI>
2018-04-10 20:09:32 +0800

    home space

--- src/Entity/Home1.java
+++ src/Entity/Home1.java
@@ -43,6 +43,9 @@ public class Home1 extends javax.swing.JFrame {
             } catch(Exception e){
                                 }
             }
+            
+            
+            
         };
         
    }
@@ -301,6 +304,7 @@ public class Home1 extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
